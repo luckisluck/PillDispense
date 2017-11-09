01b7da170de92b6d2944c278e8680d4dfd992599
Pill <luckisluck7235@gmail.com>
2017-11-09 17:29:56 +0700

    a

--- add.php
+++ add.php
@@ -1,18 +1,30 @@
-<?php
+ <?php
+$servername = "35.189.32.80";
+$username = "test";
+$password = "test";
+$dbname = "pilldispenser";
 
-$sql =CREATE TABLE `people` (
-`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`name` VARCHAR( 100 ) NOT NULL ,
-`sex` BOOL NOT NULL DEFAULT '1',
-`birthyear` INT NOT NULL
-)
-
-if (query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} 
-
-else {
-    echo "Error creating table:";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-?>
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?> 
