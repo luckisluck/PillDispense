1ed63286906a360083d07363390baf382446a836
Pill <luckisluck7235@gmail.com>
2017-11-13 18:18:01 +0700

    nn

--- dispense.php
+++ dispense.php
@@ -14,7 +14,8 @@ catch(PDOException $e)
     	die("OOPs something went wrong");
     }
 
- // Check whether username or password is set from android	
+ // Check whether username or password is set from android
+     echo "Hello";	
      if(isset($_POST['dispenseX']))
      {
 		  // Innitialize Variable
@@ -22,60 +23,16 @@ catch(PDOException $e)
 	   	  $dispenseX = $_POST['dispenseX'];
 		  
 		  // Query database for row exist or not
-          $sql = "INSERT INTO dispense (dispenseX)VALUES ('$dispenseX')";
-          if ($conn->query($sqli) === TRUE) 
+          $sql = "INSERT INTO dispense (dispenseX) VALUES ('$dispenseX')";
+          if ($conn->query($sql) === TRUE) 
             {
-            echo "X successfully";
+            $result="true";
             } 
             else 
             {
-           echo "Error: " . $sqli . "<br>" . $conn->error;
+            $result= "false";
             }
 		  
-		  // send result back to android
-   		  echo $result;
   	}
-
-       if(isset($_POST['dispenseY']))
-     {
-		  // Innitialize Variable
-		  $result='';
-	   	  $dispenseX = $_POST['dispenseY'];
-		  
-		  // Query database for row exist or not
-          $sql = "INSERT INTO dispense (dispenseY)VALUES (:dispenseY)";
-          if ($conn->query($sqli) === TRUE) 
-            {
-            echo "Y successfully";
-            } 
-            else 
-            {
-            echo "Error: " . $sqli . "<br>" . $conn->error;
-            }
-		  
-		  
-   		  
-  	}
-
-
-       if(isset($_POST['dispenseZ']))
-     {
-		  // Innitialize Variable
-		  $result='';
-	   	  $dispenseX = $_POST['dispenseZ'];
-		  
-		  // Query database for row exist or not
-          $sql = "INSERT INTO dispense (dispenseZ)VALUES (:dispenseZ)";
-          if ($conn->query($sqli) === TRUE) 
-            {
-            echo "Z successfully";
-            } 
-            else 
-            {
-            echo "Error: " . $sqli . "<br>" . $conn->error;
-            }
-   		  
-  	}
-
 
 ?>
