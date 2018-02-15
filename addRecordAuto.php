26109d88a54975b82bf11a37f9d85fd59f6a8f19
Pill <luckisluck7235@gmail.com>
2018-02-16 00:14:53 +0700

    l
    l

--- addRecordAuto.php
+++ addRecordAuto.php
@@ -9,13 +9,13 @@ $con=mysqli_connect($servername,$username,$password,$dbname);
 // Check connection
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  $result = "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
 
  // Check whether username or password is set from android
     	
-     if(isset($_POST['name'])  )
+     if(isset($_POST['name']))
      {
 		  // Innitialize Variable
 		  $result='';
