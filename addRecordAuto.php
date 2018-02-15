8aec3328bb463ac63baf7d7f1326d38eba863011
Pill <luckisluck7235@gmail.com>
2018-02-15 22:02:26 +0700

    k
    k

--- addRecordAuto.php
+++ addRecordAuto.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 
  // Check whether username or password is set from android
     	
-     if(isset($_POST['name']) && $_POST['startTime']) && $_POST['endTime']) && $_POST['pillAmt']) && $_POST['table']) )
+     if(isset($_POST['name']) && $_POST['startTime']) && $_POST['endTime']) && $_POST['pillAmt']) )
      {
 		  // Innitialize Variable
 		  $result='';
@@ -21,8 +21,8 @@ if ($conn->connect_error) {
           $startTime = $_POST['startTime'];
           $endTime = $_POST['endTime'];
           $pillAmt = $_POST['pillAmt'];
-          $table = $_POST['table'];
-		  
+   //       $table = $_POST['table'];
+		  $table='dispenseauto_X';
 		  // Query database for row exist or not
           $sql = "INSERT INTO $table (timeName,startTime,endTime,pillAmt) VALUES ('$name','$startTime','$endTime','$pillAmt')";
           if ($conn->query($sql) === TRUE) 
