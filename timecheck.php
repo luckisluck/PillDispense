c14682ff981ff78621146762779e3e47f0521c43
Pill <luckisluck7235@gmail.com>
2018-02-24 23:54:29 +0700

    test
    test

---
+++ timecheck.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('Asia/Bangkok');
+$servername = "35.189.32.80";
+$username = "test";
+$password = "test";
+$dbname = "pilldispenser";
+
+$con=mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+ // Check whether username or password is set from android
+    $time=  date("H:i:00");
+    echo $time;
+    
+?>
