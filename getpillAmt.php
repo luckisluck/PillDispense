07ed04d9f102e5751bb4bbcbf0c0c0104d67b52f
Pill <luckisluck7235@gmail.com>
2018-04-12 15:54:07 +0700

    getpill

---
+++ getpillAmt.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "35.189.32.80";
+$username = "test";
+$password = "test";
+$dbname = "pilldispenser";
+
+$con=mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+
+ $sql = "SELECT * FROM dispenser_pillAmt ";
+ $rows = array();
+            $result = $con->query($sql);
+            if (mysqli_num_rows($result) > 0) 
+                  {
+                  while($r = mysqli_fetch_assoc($result))
+                    {
+                    $rows['allp'][]=$r;
+                    }
+                  print json_encode($rows);     
+                  }
+?>
