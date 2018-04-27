091c10587f9fab35cacd5b07d7438d96980db875
Pill <luckisluck7235@gmail.com>
2018-04-27 20:21:45 +0700

    test
    test

---
+++ delete.php
@@ -0,0 +1,32 @@
+<?php
+date_default_timezone_set('Asia/Bangkok');
+$servername = "35.189.32.80";
+$username = "test";
+$password = "test";
+$dbname = "pilldispenser";
+
+$con=mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  $a= "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+          if(  (isset($_POST['name'])) && (isset($_POST['table']))  )
+                {
+                $Name= $_POST['name'];
+                $table=$_POST['table'];
+                $insertX ="DELETE FROM $table WHERE timeName=$Name";
+                  if ($con->query($insertX) === TRUE) 
+                        {
+                        $results="true";
+                        } 
+                  else 
+                        {
+                        $results= "false";
+                        }
+                }
+                  
+                  echo $result;
+            }
+?>
