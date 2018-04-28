f7bebabdb90a96918ca67e2dc1fcaafdc089c311
Pill <luckisluck7235@gmail.com>
2018-04-28 18:23:30 +0700

    Test Sync
    Sync

---
+++ getallconfig.php
@@ -0,0 +1,50 @@
+<?php
+$servername = "35.189.32.80";
+$username = "test";
+$password = "test";
+$dbname = "pilldispenser";
+
+$con=mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+
+ $sql = "SELECT * FROM dispenseauto_X ";
+ $sql1 = "SELECT * FROM dispenseauto_Y ";
+ $sql2 = "SELECT * FROM dispenseauto_Z ";
+
+ $rows = array();
+            $result = $con->query($sql);
+            $result1 = $con->query($sql1);
+            $result2 = $con->query($sql2);
+
+            if (mysqli_num_rows($result) > 0 ) 
+                  {
+                  while($r = mysqli_fetch_assoc($result))
+                    {
+                    $rows['pillX'][]=$r;
+                    }    
+                  }
+
+            if (mysqli_num_rows($result1) > 0 ) 
+                  {
+                  while($r = mysqli_fetch_assoc($result1))
+                    {
+                    $rows['pillY'][]=$r;
+                    }    
+                  }
+
+            if (mysqli_num_rows($result2) > 0 ) 
+                  {
+                  while($r = mysqli_fetch_assoc($result2))
+                    {
+                    $rows['pillZ'][]=$r;
+                    }    
+                  }
+
+             print json_encode($rows);
+?>
